cd9040ee97e9854a9c2b3719f5a5db1353ddc795
mac <mac@MacBook-Pro.local>
2019-01-27 20:04:18 -0200

    App gerador de numeros da mega sena, ios Swift

--- Loteria/ViewController.swift
+++ Loteria/ViewController.swift
@@ -8,13 +8,62 @@
 
 import UIKit
 
+enum GameType: String{
+    case megasena = "Mega-Sena"
+    case quina = "Quina"
+}
+
+infix operator >-<
+func >-< (total: Int, universe: Int) -> [Int] {
+    var result: [Int] = []
+    while result.count  < total {
+        let randomNumber = Int(arc4random_uniform(UInt32(universe))+1)
+        if !result.contains(randomNumber){
+            result.append(randomNumber)
+        }
+    }
+    return result.sorted()
+}
+
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbGameType: UILabel!
+    @IBOutlet weak var scGameType: UISegmentedControl!
+     @IBOutlet var balls: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        showNumbers(for: .megasena)
     }
+    
+    func showNumbers(for type: GameType) {
+       lbGameType.text = type.rawValue
+        var game: [Int] = []
+        switch type {
+        case .megasena:
+        game = 6>-<60
+           balls.last!.isHidden = false
+        case .quina:
+        game = 5>-<80
+           balls.last!.isHidden = true
+        }
+        
+        for (index, game) in game.enumerated() {
+            balls[index].setTitle("\(game)", for: .normal)
+        }
+    }
+    
 
-
+    @IBAction func generateGame() {
+        switch scGameType.selectedSegmentIndex {
+        case 0:
+            showNumbers(for: .megasena)
+        default:
+            showNumbers(for: .quina)
+        }
+    }
 }
 
