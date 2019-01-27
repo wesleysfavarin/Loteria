f2abd7dbbb0b6a46f2fe82696bb3847f79cbb9bb
mac <mac@macbook-pro.local>
2019-01-27 17:10:32 -0200

    Initial Commit

---
+++ Loteria/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Loteria
+//
+//  Created by mac on 27/01/19.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
